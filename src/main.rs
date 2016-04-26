f94f2745ad3369fe675ea47f89ace5d78de1b1ac
cheezgi <zackh@firelmail.cc>
2016-04-26 19:45:33 -0400

    todo

--- src/main.rs
+++ src/main.rs
@@ -148,6 +148,7 @@ fn main() {
                 }
             }
             else {
+                // TODO: make this not ridiculous
                 let re = Regex::new(r"movies").unwrap();
                 if !re.is_match(&i) {
                     println!("Not an option: {}", i);
