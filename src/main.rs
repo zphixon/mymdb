a49eacecdd9f40b93af6dd6a7861be835ef638f1
Zack H <zackh@firemail.cc>
2016-07-16 15:18:29 +0000

    add comments for future sanity

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ use std::path::Path;
 use std::env;
 use regex::Regex;
 
+// movie struct
 #[derive(Debug)]
 struct Movie {
     id: i32,
@@ -23,7 +24,9 @@ struct Movie {
     rating: i32
 }
 
+// movie impl: possibly add search functions
 impl Movie {
+    // create new movie
     fn new(id: i32, name: String, time_created: Timespec, opinion: String, rating: i32) -> Movie {
         Movie {
             id: id,
@@ -33,39 +36,22 @@ impl Movie {
             rating: rating,
         }
     }
-
-    /*
-    fn get_name(self) -> String {
-        self.name
-    }
-
-    fn get_opinion(self) -> String {
-        self.opinion
-    }
-
-    fn get_id(self) -> i32 {
-        self.id
-    }
-
-    fn get_rating(self) -> i32 {
-        self.rating
-    }
-
-    fn get_time(self) -> Timespec {
-        self.time_created
-    }
-    */
 }
 
 fn main() {
+    // open connection to .movies.db in current directory: possibly change to
+    // ~/.movies.db for simplicity
     let conn = Connection::open(Path::new(".movies.db")).unwrap();
 
+    // create a table in movies database that maps to a movie struct
     conn.execute("CREATE TABLE IF NOT EXISTS movies (
         id INTEGER UNIQUE NOT NULL,
         name TEXT NOT NULL,
         time_created TEXT NOT NULL,
         opinion TEXT NOT NULL,
         rating INTEGER)", &[]).unwrap();
+
+    // get args
     let args: Vec<String> = env::args().collect();
     let length = args.len();
 
@@ -75,17 +61,23 @@ fn main() {
     else if length == 2 {
         for i in &args {
             if i == "help" { print_help(); }
-            else if i == "add" { 
+            else if i == "add" {
+                // prompt user for new movie
                 let new_movie = new_movie();
+
+                // insert new movie into database
                 conn.execute("INSERT INTO movies VALUES ($1, $2, $3, $4, $5)",
                     &[&new_movie.id, &new_movie.name, &new_movie.time_created,
                     &new_movie.opinion, &new_movie.rating]).unwrap();
+
                 println!("Your movie has been added. ID# {}", &new_movie.id);
             }
-            else if i == "remove" { 
+            else if i == "remove" {
+                // get movie id
                 println!("ID of movie to be removed: (number)");
                 let id: i32 = number_input();
 
+                // find movie by id: TODO: make more efficient
                 let mut stmt = conn.prepare("SELECT * FROM movies WHERE id=$1").unwrap();
                 let movie_iter = stmt.query_map(&[&id,], |row| {
                     Movie {
@@ -97,6 +89,8 @@ fn main() {
                     }
                 }).unwrap();
 
+                // get last movie with id because shitty API or I don't know how to use it
+                // probably the latter honestly
                 let mut q = Movie::new(0, String::new(), time::get_time(), String::new(), 0);
                 for i in movie_iter {
                     q = i.unwrap();
@@ -109,10 +103,9 @@ fn main() {
 
                 println!("Are you sure you want to remove {}?", q.name.trim());
 
-                let resp1 = get_input();
-                let resp2 = resp1.to_lowercase();
-                let resp = resp2.trim();
+                let resp = get_input().to_lowercase().trim();
 
+                // remove from database
                 if resp == String::from("yes") {
                     conn.execute("DELETE FROM movies WHERE id=$1", &[&id,]).unwrap();
                     println!("{} has been removed from the database.", q.name.trim());
@@ -123,7 +116,9 @@ fn main() {
                     println!("Movie will not be removed.");
                 }
             }
+            // show movies in database
             else if i == "show" {
+                // select all movies, conver to iterator
                 let mut stmt = conn.prepare("SELECT * FROM movies").unwrap();
                 let movie_iter = stmt.query_map(&[], |row| {
                     Movie {
@@ -135,6 +130,7 @@ fn main() {
                     }
                 }).unwrap();
 
+                // convert list of movies to vector
                 let mut count = 0;
                 let mut movies: Vec<Movie> = vec![];
                 for movie in movie_iter {
@@ -144,6 +140,7 @@ fn main() {
 
                 println!("Found {} movie(s)", count);
 
+                // print movies
                 for z in movies {
                     println!("Name:    {}\nOpinion: {}\nRating:  {}\nID:      {}\nTime:    {}\n",
                              z.name.trim(),
@@ -165,6 +162,7 @@ fn main() {
         }
     }
     else {
+        // TODO: get argparse or something like that
         let mut arg3: &String = &String::from("");
         let mut arg4: &String = &String::from("");
         let mut arg5: i32 = 0;
@@ -177,6 +175,7 @@ fn main() {
         }
         for i in &args {
             if i == "-a" {
+                // add movie
                 let id = rand::random::<i32>().abs() / 1000;
                 conn.execute("INSERT INTO movies VALUES ($1, $2, $3, $4, $5)",
                     &[&id, arg3, &(time::get_time()),
@@ -184,6 +183,7 @@ fn main() {
                 println!("Your movie has been added. ID# {}", id);
             }
             else if i == "-r" {
+                // remove movie
                 let id: i32 = arg3.trim().parse().expect("arg must be a number");
 
                 let mut stmt = conn.prepare("SELECT * FROM movies WHERE id=$1").unwrap();
@@ -214,6 +214,7 @@ fn main() {
     }
 }
 
+// create new movie struct using input
 fn new_movie() -> Movie {
     println!("Name of movie:");
     let name: String = get_input();
@@ -224,10 +225,12 @@ fn new_movie() -> Movie {
     println!("Rating: (number)");
     let rating: i32 = number_input();
 
+    // TODO: non-retarded ID numbers, like autoincrement
     Movie::new(rand::random::<i32>().abs() / 1000, name,
         time::get_time(), opinion, rating)
 }
 
+// return time as string
 fn time(t: Timespec) -> String {
     let real_time = time::at(t);
     String::from(format!("{}-{}-{} {}:{}", (real_time.tm_year + 1900), real_time.tm_mon,
@@ -250,6 +253,7 @@ fn print_help() {
     println!("not exist, it is created automatically.");
 }
 
+// get input as number
 fn number_input() -> i32 {
     let n = match get_input().trim().parse::<i32>() {
         Ok(n) => n,
@@ -259,6 +263,7 @@ fn number_input() -> i32 {
     n
 }
 
+// get input as string
 fn get_input() -> String {
     let mut i = String::new();
     let handle = io::stdin();
